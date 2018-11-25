1cd869fc3d254daa1d774a92bcbd2740da31eb77
Jaime Ginorio <jaime.ginorio@upr.edu>
2018-11-24 21:54:48 -0400

    First Commit

---
+++ JaimeMVC/app/config/config.php
@@ -0,0 +1,16 @@
+<?php
+    //DB Params
+    define('DB_HOST', '127.0.0.1');
+    define('DB_USER', '_YOUR_USER_');
+    //Buscar si esto se hace asi explicitamente, si es seguro
+    define('DB_PASS', '_YOUR_PASS_');
+    define('DB_NAME', '_YOUR_DBNAME_');
+    
+
+
+    //App Root
+    define('APPROOT',dirname(dirname(__FILE__)));
+    //URL Root
+    define('URLROOT', '_YOUR_URL_');
+    // Site name
+    define('SITENAME', '_YOUR_SITENAME');
